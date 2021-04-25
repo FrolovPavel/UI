98f18afd59b67fef368504890400d405575093e6
Pavel Frolov <Frolov-Works@yandex.ru>
2021-04-25 18:38:55 +0300

    Accordion

---
+++ Accordion/script.js
@@ -0,0 +1,18 @@
+const accordions = document.querySelectorAll('.accordion__item')
+
+accordions.forEach((item) => {
+      item.addEventListener('click', (e) => {
+
+          const self = e.currentTarget;
+          const control = self.querySelector('.accordion__control');
+          const content = self.querySelector('.accordion__content');
+
+          self.classList.toggle('open')
+
+          if(self.classList.contains('open')) {
+              content.style.maxHeight = content.scrollHeight + 'px';
+          } else {
+              content.style.maxHeight = null
+          }
+      })
+})
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # UI
-## custom style for checkbox and radio form elements
+## Accordion on JS
+![2021-04-25 18-14-16_Trim](https://user-images.githubusercontent.com/73355761/115999284-dd197080-a5f3-11eb-95f3-b1d31c6b856f.gif)
+## Custom style for checkbox and radio form elements
 ![2021-04-08 21-13-15_Trim](https://user-images.githubusercontent.com/73355761/114076721-ab906d80-98af-11eb-9ddd-d9b8462adb62.gif)
-## wave-button
+## Wave-button
 ![2021-04-06 14-28-45 (1)](https://user-images.githubusercontent.com/73355761/113727807-24e85e80-96fe-11eb-8e57-002e680764dc.gif)
